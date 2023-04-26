9fc21ba925623eafec8cdef213a01c5b3437d8b6
Anzhela Bazalinska <anzhela.bazalinskaya@gmail.com>
2023-04-26 14:16:59 +0300

    Update fetchCountries.js

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -2,12 +2,9 @@ const BASE_URL = 'https://restcountries.com/v3.1/name/';
 const fields = 'fields=name,capital,population,flags,languages';
 
 function fetchCountries(name) {
-  return fetch(`${BASE_URL}${name}?${fields}`).then(response => {
-    if (!response.ok) {
-      throw new Error(response.status);
-    }
-    return response.json();
-  });
+  return fetch(`${BASE_URL}${name}?${fields}`).then(response =>
+    response.json()
+  );
 }
 
 export { fetchCountries };
